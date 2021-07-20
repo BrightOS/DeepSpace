b14c33e0d316d20f1acc4cfd062cf64afcb5c057
Abubakirov Vladimir <abubakirov04@mail.ru>
2021-07-20 21:47:21 +0300

    Added functions to viewModel of registration

--- app/src/main/java/ru/myitschool/nasa_bootcamp/ui/registration/RegistrationFragment.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/ui/registration/RegistrationFragment.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import ru.myitschool.nasa_bootcamp.databinding.FragmentRegistrationBinding
--- app/src/main/java/ru/myitschool/nasa_bootcamp/ui/registration/RegistrationViewModel.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/ui/registration/RegistrationViewModel.kt
@@ -1,4 +1,7 @@
 package ru.myitschool.nasa_bootcamp.ui.registration
 
 interface RegistrationViewModel {
+    suspend fun CreateUser(email: String, password: String)
+    suspend fun AuthenticateUser(email: String, password: String)
+    suspend fun SignOutUser()
 }
