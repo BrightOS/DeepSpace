34147ab766125d47699d4927899d731809d777a7
YanaGlad <olesya_leader@mail.ru>
2021-07-20 19:29:12 +0300

    Добавлены модели запусков SpaceX, MarsRovers, Apod

--- app/src/main/java/ru/myitschool/nasa_bootcamp/MainActivity.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/MainActivity.kt
@@ -2,10 +2,13 @@ package ru.myitschool.nasa_bootcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
