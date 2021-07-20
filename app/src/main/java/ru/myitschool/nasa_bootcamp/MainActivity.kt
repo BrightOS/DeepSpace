deb6564c615933b32184c1e8b6520d9cbbcd97f8
YanaGlad <olesya_leader@mail.ru>
2021-07-20 12:48:05 +0300

    Добавила модель запусков SpaceX, ViewModels

--- app/src/main/java/ru/myitschool/nasa_bootcamp/MainActivity.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.myitschool.nasa_bootcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.myitschool.nasa_bootcamp.viewModel.SxLauchViewModel
 
 class MainActivity : AppCompatActivity() {
+    val sxLauchViewModel: SxLauchViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sxLauchViewModel.viewModelScope.launch {
+            sxLauchViewModel.loadLauches()
+        }
     }
 }
--- app/src/main/java/ru/myitschool/nasa_bootcamp/model/Camera.kt
+++
@@ -1,9 +0,0 @@
-package com.example.firstkotlinapp.model
-
-import com.google.gson.annotations.SerializedName
-
-class Camera( //Название и полное название камеры
-      @field:SerializedName("name") val name: String,
-      @field:SerializedName("fullname") val fullname: String
-
-)
