4d170b4d59340a7cdee4a7a8e6f5d4a15951baec
BrightOS <dbarov3@gmail.com>
2021-07-27 19:24:18 +0500

    create(DimensionsUtil.kt) utility that can convert px to dp and vice versa, programmatically set indents for the View and get the height of the NavigationBar and StatusBar

---
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/utils/DimensionsUtil.kt
@@ -0,0 +1,46 @@
+package ru.myitschool.nasa_bootcamp.utils
+
+import android.content.Context
+import android.content.res.Resources
+import android.util.DisplayMetrics
+import android.view.View
+import android.view.ViewGroup
+import kotlin.math.roundToInt
+
+object DimensionsUtil {
+    fun pxToDp(context: Context, px: Int): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return (px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)).roundToInt()
+    }
+
+    fun dpToPx(context: Context, dp: Int): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return (dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)).roundToInt()
+    }
+
+    fun getStatusBarHeight(resources: Resources): Int {
+        var result = 0
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = resources.getDimensionPixelSize(resourceId)
+        }
+        return result
+    }
+
+    fun getNavigationBarHeight(resources: Resources): Int {
+        var result = 0
+        val resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = resources.getDimensionPixelSize(resourceId)
+        }
+        return result
+    }
+
+    fun setMargins(view: View, left: Int, top: Int, right: Int, bottom: Int) {
+        if (view.layoutParams is ViewGroup.MarginLayoutParams) {
+            val p = view.layoutParams as ViewGroup.MarginLayoutParams
+            p.setMargins(left, top, right, bottom)
+            view.requestLayout()
+        }
+    }
+}
