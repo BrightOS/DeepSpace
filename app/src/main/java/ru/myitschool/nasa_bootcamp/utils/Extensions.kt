500344dd1fcec39286722dcbe57ee9404ff3b008
BrightOS <dbarov3@gmail.com>
2021-07-27 19:25:02 +0500

    feature(Extensions.kt) add a function to get the color from the attributes

--- app/src/main/java/ru/myitschool/nasa_bootcamp/utils/Extensions.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/utils/Extensions.kt
@@ -1,5 +1,9 @@
 package ru.myitschool.nasa_bootcamp.utils
 
+import android.content.Context
+import android.content.res.Resources
+import android.util.TypedValue
+import androidx.annotation.ColorInt
 import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doOnTextChanged
 import com.google.android.material.textfield.TextInputEditText
@@ -10,5 +14,13 @@ object Extensions {
         doOnTextChanged{ text, start, before, count ->
             inputLayout.isErrorEnabled = false
         }
+    }
+
+    @ColorInt
+    fun getColorFromAttributes(context: Context, resID: Int): Int {
+        val typedValue = TypedValue()
+        val theme: Resources.Theme = context.theme!!
+        theme.resolveAttribute(resID, typedValue, true)
+        return typedValue.data
     }
 }
