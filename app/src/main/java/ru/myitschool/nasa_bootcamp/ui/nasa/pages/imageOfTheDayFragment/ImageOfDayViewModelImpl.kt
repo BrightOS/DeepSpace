5c867e75382d0f4bb2918d1936990dac28b1ea4c
YanaGlad <olesya_leader@mail.ru>
2021-07-27 17:38:34 +0300

    Merged Nasa repositories

--- app/src/main/java/ru/myitschool/nasa_bootcamp/ui/nasa/pages/imageOfTheDayFragment/ImageOfDayViewModelImpl.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/ui/nasa/pages/imageOfTheDayFragment/ImageOfDayViewModelImpl.kt
@@ -3,16 +3,15 @@ package ru.myitschool.nasa_bootcamp.ui.nasa.pages.imageOfTheDayFragment
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import ru.myitschool.nasa_bootcamp.data.repository.ImageOfDayRepository
+import ru.myitschool.nasa_bootcamp.data.repository.NasaRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class ImageOfDayViewModelImpl @Inject constructor(
-    private val nasaRepo : ImageOfDayRepository
+    private val nasaRepo : NasaRepository
 ) : ViewModel(), ImageOfDayViewModel {
 
     suspend fun loadImageOfDay(){
-
         val response = nasaRepo.getImageOfTheDay()
         Log.d("TAGTAG_A", response.body()!!.url)
     }
