92bfb2111cdf626cfd4d430dad4d5ca230a8b682
YanaGlad <olesya_leader@mail.ru>
2021-07-28 20:35:50 +0300

    Getting info from NewsViewmodel

--- app/src/main/java/ru/myitschool/nasa_bootcamp/ui/nasa/pages/newsFragment/NewsFragment.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/ui/nasa/pages/newsFragment/NewsFragment.kt
@@ -1,60 +1,41 @@
 package ru.myitschool.nasa_bootcamp.ui.nasa.pages.newsFragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import kotlinx.coroutines.launch
 import ru.myitschool.nasa_bootcamp.R
+import ru.myitschool.nasa_bootcamp.ui.spacex.SpaceXLaunchAdapter
+import ru.myitschool.nasa_bootcamp.ui.spacex.SpaceXViewModel
+import ru.myitschool.nasa_bootcamp.ui.spacex.SpaceXViewModelImpl
+import java.util.Observer
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NewsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NewsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val newsViewModel: NewsViewModel by viewModels<NewsViewModelIml>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+        newsViewModel.getViewModelScope().launch {
+            newsViewModel.getNews()
+        }
+
+
+        newsViewModel.getArticlesList().observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+
+        })
+
         return inflater.inflate(R.layout.fragment_news, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/ru/myitschool/nasa_bootcamp/ui/nasa/pages/newsFragment/NewsViewModel.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/ui/nasa/pages/newsFragment/NewsViewModel.kt
@@ -1,7 +1,15 @@
 package ru.myitschool.nasa_bootcamp.ui.nasa.pages.newsFragment
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import ru.myitschool.nasa_bootcamp.data.dto.news.Article
+import ru.myitschool.nasa_bootcamp.data.model.ArticleModel
+import ru.myitschool.nasa_bootcamp.data.model.SxLaunchModel
 
 interface NewsViewModel {
     suspend fun getNews()
+    fun getViewModelScope(): CoroutineScope
+    fun getArticlesList(): MutableLiveData<ArrayList<ArticleModel>>
+
 }
