d64bf8084ed67212cfdc578528b36a62c41b13b8
YanaGlad <olesya_leader@mail.ru>
2021-07-28 16:28:46 +0300

    Some fixes to LaunchItem

--- app/src/main/java/ru/myitschool/nasa_bootcamp/data/dto/spaceX/launches/Core.kt
+++
@@ -1,9 +0,0 @@
-package ru.myitschool.nasa_bootcamp.data.dto.spaceX.launches
-
-//class Core(
-//    @field:SerializedName("cores") val cores: List<Core>,
-//    @field:SerializedName("cores") val cores: List<Core>,
-//    @field:SerializedName("cores") val cores: List<Core>,
-//    @field:SerializedName("cores") val cores: List<Core>,
-//
-//    )
