c6fd9960835c208b426b8b27a563991f7a9cbc41
YanaGlad <olesya_leader@mail.ru>
2021-07-28 22:23:46 +0300

    Observing viewModels

--- app/src/main/java/ru/myitschool/nasa_bootcamp/ui/spacex/explore/cores/CoresFragment.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/ui/spacex/explore/cores/CoresFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import ru.myitschool.nasa_bootcamp.R
+import ru.myitschool.nasa_bootcamp.data.dto.spaceX.cores.Core
+import ru.myitschool.nasa_bootcamp.ui.spacex.explore.capsules.CapsulesViewModel
+import ru.myitschool.nasa_bootcamp.ui.spacex.explore.capsules.CapsulesViewModelImpl
 
 @AndroidEntryPoint
 class CoresFragment : Fragment() {
+
+    private val coresViewModel: CoresViewModel by viewModels<CoresViewModelImp>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +27,14 @@ class CoresFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        coresViewModel.getViewModelScope().launch {
+            coresViewModel.getCores()
+        }
+
+        coresViewModel.getCoresList().observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+
+        })
 
         return inflater.inflate(R.layout.fragment_cores, container, false)
     }
--- app/src/main/java/ru/myitschool/nasa_bootcamp/ui/spacex/explore/roadster/RoadsterFragment.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/ui/spacex/explore/roadster/RoadsterFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import kotlinx.coroutines.launch
 import ru.myitschool.nasa_bootcamp.R
+import ru.myitschool.nasa_bootcamp.ui.spacex.explore.history.HistoryViewModel
+import ru.myitschool.nasa_bootcamp.ui.spacex.explore.history.HistoryViewModelImpl
 
 
 class RoadsterFragment : Fragment() {
+    private val roadsterViewModel: RoadsterViewModel by viewModels<RoadsterViewModelImpl>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,15 @@ class RoadsterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
+        roadsterViewModel.getViewModelScope().launch {
+            roadsterViewModel.getRoadsterInfo()
+        }
+
+        roadsterViewModel.getRoadsterModel().observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+
+        })
 
         return inflater.inflate(R.layout.fragment_roadster, container, false)
     }
