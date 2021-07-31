a2590dd4bad70e6aa58fe7c282b714887659ba4a
YanaGlad <olesya_leader@mail.ru>
2021-08-01 00:43:10 +0300

    Added ErrorHandler, progressBar observing

--- app/src/main/java/ru/myitschool/nasa_bootcamp/data/dto/spaceX/history/History.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/data/dto/spaceX/history/History.kt
@@ -6,12 +6,12 @@ import ru.myitschool.nasa_bootcamp.data.model.HistoryModel
 class History(
     @field:SerializedName("id") val id: Int,
     @field:SerializedName("title") val title: String,
-    @field:SerializedName("event_date_utc") val event_date_utc: String,
+    @field:SerializedName("event_date_unix") val event_date_unix: Int,
     @field:SerializedName("details") val details: String
 ) {
 
     public fun createHistoryModel(): HistoryModel {
-        return HistoryModel(id, title, event_date_utc, details)
+        return HistoryModel(id, title, event_date_unix, details)
     }
 }
 ///Можно добавить ссылки на википедию/сайт spaceX
