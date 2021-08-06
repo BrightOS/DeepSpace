6f326f025fea2ac350819be4f04d207fce609fd4
Abubakirov Vladimir <abubakirov04@mail.ru>
2021-08-06 23:52:55 +0300

    Connected listener

--- app/src/main/java/ru/myitschool/nasa_bootcamp/data/repository/NetworkRepositoryImpl.kt
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/data/repository/NetworkRepositoryImpl.kt
@@ -10,40 +10,18 @@ import javax.inject.Inject
 
 class NetworkRepositoryImpl @Inject constructor(
     private val firebaseRepository: FirebaseRepository,
-    newsRepository: NewsRepository
+    private val newsRepository: NewsRepository
 ) : NetworkRepository {
     override suspend fun getNews(): Resource<List<LiveData<ContentWithLikesAndComments<ArticleModel>>>> {
-
+        val newsList = mutableListOf<LiveData<ContentWithLikesAndComments<ArticleModel>>>()
+        for (news in newsRepository.getNews().body()!!) {
+            val data = MutableLiveData<ContentWithLikesAndComments<ArticleModel>>()
+            data.postValue(ContentWithLikesAndComments(ArticleModel(news.id.toLong(), news.title, news.url, news.newsSite, news.summary, news.publishedAt, news.updatedAt), listOf(), listOf()))
+            firebaseRepository.articleModelEventListener(data)
+            newsList.add(data)
+        }
         return Resource.success(
-            listOf(
-                MutableLiveData(
-                    ContentWithLikesAndComments(
-                        likes = listOf(),
-                        comments =
-                        listOf(
-                            Comment(
-                                text = "ds/lkfsdfksdfsd",
-                                subComments = listOf(),
-                                id = 4,
-                                author = UserModel(
-                                    id = "4",
-                                    avatarUrl = Uri.parse("https://lh3.googleusercontent.com/0xn6EsKc4lfdgFBt_1rA8uN6FgUUCrNO7cmTQny30x6wQhFrlTuZomwENpYsyMW00lytSuv6hLSHOs1voqpUautXcQ"),
-                                    name = "Zach"
-                                ),
-                                likes = listOf(),
-                                date = 1628008932
-                            )
-                        ),
-                        content = ArticleModel(
-                            id = 123,
-                            title = "Hello World",
-                            summary = "Hmm d;lfksd ;fkhsa ldkhf;s aldkhfl sdjfaskdhalsdl asdak  sdhlas khdlask dlkasdas ldkhas/ ldkha /sldkh a/skd a/skda skhdas",
-                            imageUrl = "https://i.ytimg.com/vi/0QaX4KMjeVQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDEYHd9BOxp4Qnw0gdFKQe2fNf1xg",
-                            publishedAt = "2021-08-03T20:40:02.000Z"
-                        )
-                    )
-                )
-            )
+            newsList
         )
     }
 
