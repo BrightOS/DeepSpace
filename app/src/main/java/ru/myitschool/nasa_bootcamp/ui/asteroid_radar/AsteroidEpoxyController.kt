d934adbdca2ec6123763a79478137fb2e9a3b6af
BrightOS <dbarov3@gmail.com>
2021-08-07 18:52:24 +0500

    create(AsteroidEpoxyController.kt) controller for recyclerview in asteroids fragment

---
+++ app/src/main/java/ru/myitschool/nasa_bootcamp/ui/asteroid_radar/AsteroidEpoxyController.kt
@@ -0,0 +1,79 @@
+package ru.myitschool.nasa_bootcamp.ui.asteroid_radar
+
+import android.content.Context
+import android.view.View
+import androidx.core.view.ViewCompat
+import com.airbnb.epoxy.EpoxyController
+import ru.myitschool.nasa_bootcamp.data.model.AsteroidModel
+
+class AsteroidEpoxyController(
+    val context: Context
+) : EpoxyController() {
+
+    val data: MutableList<AsteroidModel> = mutableListOf()
+    val isSticky: MutableList<Boolean> = mutableListOf()
+    private var currentDate = ""
+
+    fun setData(data: MutableList<AsteroidModel>?) {
+        this.data.clear()
+
+        addData(data)
+    }
+
+    fun addData(data: MutableList<AsteroidModel>?) {
+        if (data != null) {
+            this.data.addAll(data)
+
+            var previousDate = ""
+            data.forEach {
+                if (it.date != previousDate) {
+                    previousDate = it.date
+                    isSticky.add(true)
+                }
+                isSticky.add(false)
+            }
+        }
+
+        requestModelBuild()
+    }
+
+    override fun isStickyHeader(position: Int) = isSticky[position]
+
+    override fun setupStickyHeaderView(stickyHeader: View) {
+        super.setupStickyHeaderView(stickyHeader)
+        stickyHeader.elevation = 15000f
+    }
+
+    override fun teardownStickyHeaderView(stickyHeader: View) {
+        super.teardownStickyHeaderView(stickyHeader)
+        stickyHeader.elevation = 0f
+    }
+
+    override fun buildModels() {
+        if (data.size > 0) {
+            var dateID = -1
+            val list = data.toList()
+
+            list.forEach {
+                if (it.date != currentDate) {
+                    asteroidDate {
+                        id(Long.MAX_VALUE - dateID)
+                        dateID++
+                        date(it.date)
+                    }
+                    currentDate = it.date
+                }
+
+                asteroid(context) {
+                    id(it.id)
+                    name(it.name)
+                    estimatedDiameter(it.estimatedDiameter)
+                    absoluteMagnitude(it.absoluteMagnitude)
+                    speed(it.speed)
+                    distanceFromEarth(it.distanceFromEarth)
+                    potencialDanger(it.potencialDanger)
+                }
+            }
+        }
+    }
+}
